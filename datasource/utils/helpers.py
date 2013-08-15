40ae999bb3a31d5c33e4b54036d43cf65b4f6be8
Michael Palumbo <michael.palumbo87@gmail.com>
2013-08-15 16:18:41 +0200

    Moved helpers tests in doctests.

--- datasource/utils/helpers.py
+++ datasource/utils/helpers.py
@@ -1,4 +1,21 @@
 def truncate(s, size=80, tail='...'):
+    """
+    >>> truncate('abc'*20, 10, '...')
+    'abcabca...'
+
+    >>> truncate ('abc'*20, 2, '...')
+    '..'
+
+    >>> truncate('abc'*20, 0, '...')
+    ''
+
+    >>> truncate('abc'*20, 60, '...')
+    'abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc'
+
+    >>> truncate('abc'*20, 70, '...')
+    'abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc'
+    
+    """
     s_size = len(s)
     
     if s_size <= size:
@@ -8,5 +25,5 @@ def truncate(s, size=80, tail='...'):
     if tail_size > size:
         return tail[:size]
         
-    pos_max = size - len(tail)
+    pos_max = size - tail_size
     return s[:pos_max] + tail
